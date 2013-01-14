2a9d0ba05d21ce6b1da8ea59c94bb46c56f793f7
wontonst <roycraft3@gmail.com>
2013-01-14 04:21:26 +0000

    hmm

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $database_name='simplePHPaccounts';
 $mysql_server='localhost';//your server location (do not change if you
 //dont know what this is)
-$mysql_username='';//your username
+$mysql_username=''//your username
 $mysql_password='';//your password
 
 //OTHER INFORMATION
